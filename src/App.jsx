8479126cc2cc9b9c607bf19e260c844e3258e495
Krystsina Hurskaya <krista97mail.ru@mail.ru>
2026-08-29 15:47:00 +0300

    компоненты разнесены по папкам

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,6 @@
-function Header() {
-  return (
-    <header>
-      <h1>Учебный проект по REACT</h1>
-    </header>
-  )
-}
-
-function MainContent({ children }) {
-  return (
-    <main>
-      <h2>Подробнее о проекте курсах</h2>
-      {children}
-    </main>
-  )
-}
-
-function Section() {
-  return (
-    <section>
-      <p>Этот проект пишется в учебных целях в рамках обучения в REDEV.</p>
-      <p>Говорят, что здесь лучшие курсы по REACT и JS</p>
-    </section>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <p>{new Date().getFullYear()}</p>
-    </footer>
-  )
-}
-
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { MainContent, Section } from './components/Content'
 function App() {
   return (
     <div>
