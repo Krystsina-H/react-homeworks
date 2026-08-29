fe849fe076241d52e762abaaa615609b5e3c07dc
Krystsina Hurskaya <krista97mail.ru@mail.ru>
2026-08-29 15:19:13 +0300

    созданы компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+function Header() {
+  return (
+    <header>
+      <h1>Учебный проект по REACT</h1>
+    </header>
+  )
+}
+
+function MainContent({ children }) {
+  return (
+    <main>
+      <h2>Подробнее о проекте курсах</h2>
+      {children}
+    </main>
+  )
+}
+
+function Section() {
+  return (
+    <section>
+      <p>Этот проект пишется в учебных целях в рамках обучения в REDEV.</p>
+      <p>Говорят, что здесь лучшие курсы по REACT и JS</p>
+    </section>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>{new Date().getFullYear()}</p>
+    </footer>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Привет всем!</h1>
-      <p>Уто учебный React-проект. Здесь буду выполнять задания по реакту.</p>
-    </>
+    <div>
+      <Header />
+      <MainContent>
+        <Section />
+      </MainContent>
+      <Footer />
+    </div>
   )
 }
 
